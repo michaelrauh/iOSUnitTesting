1ee47e64d61034396416ad9660c297803093d829
michaelrauh <michaelrauh@ymail.com>
2017-07-03 14:24:09 -0400

    moved VC tests to quick/nimble

--- iOSUnitTestingTests/ViewControllerTests.swift
+++ iOSUnitTestingTests/ViewControllerTests.swift
@@ -1,82 +1,99 @@
-import XCTest
+import Quick
+import Nimble
 import Moxie
 @testable import iOSUnitTesting
 
-class iOSUnitTestingTests: XCTestCase {
-    var subject: ViewController!
-    var mockNavigator: MockNavigator!
-    var mockViewModel: MockExampleViewModel!
-    
-    override func setUp() {
-        super.setUp()
-        subject = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController
-        
-        mockNavigator = MockNavigator()
-        mockViewModel = MockExampleViewModel()
-        
-        subject.navigator = mockNavigator
-        subject.viewModel = mockViewModel
-    }
-    
-    func testThatALabelIsSetOnViewDidLoad() {
-        _ = subject.view
-        XCTAssertEqual(subject.someLabel.text, "This is some text")
-    }
-    
-    func testThatALabelIsSetOnButtonTap() {
-        _ = subject.view
-        subject.changeTextButton.sendActions(for: .touchUpInside)
-        XCTAssertEqual(subject.otherLabel.text, "This is different text")
-    }
-    
-    func testThatNavigatorIsCalledOnButtonTap() {
-        _ = subject.view
-        subject.otherButton.sendActions(for: .touchUpInside)
-        XCTAssertTrue(mockNavigator.invoked(function: "show", with: [subject, "otherVC"]))
-    }
-    
-    func testThatALabelCanBeSetBasedUponAViewModel() {
-        mockViewModel.stub(function: "calculate", return: 10)
-        _ = subject.view
-        XCTAssertEqual(subject.thirdLabel.text, "your number is 10")
-    }
-    
-    func testThatTouchingTheNetworkButtonHitsTheNetwork() {
-        _ = subject.view
-        subject.pokemonIndex.text = "5"
-        subject.networkButton.sendActions(for: .touchUpInside)
-        XCTAssertTrue(mockViewModel.invoked(function: "makeCall", with: [subject, "5"]))
-    }
-    
-    func testThatTouchingTheNetworkButtonWithoutSettingIndexHitsTheNetworkWithNothing() {
-        _ = subject.view
-        subject.networkButton.sendActions(for: .touchUpInside)
-        XCTAssertTrue(mockViewModel.invoked(function: "makeCall", with: [subject, ""]))
-    }
-    
-    func testThatTheViewDelegateSetsTheLabel() {
-        _ = subject.view
-        subject.viewModel.pokemonName = "Pikachu"
-        subject.pokemonIndex.text = "5"
-        subject.networkButton.sendActions(for: .touchUpInside)
-        XCTAssertTrue(mockViewModel.invoked(function: "makeCall", with: [subject, "5"]))
-        
-        // TODO: Capture the first argument of makeCall and call onSuccess on it
-        
-        subject.onSuccess()
-        XCTAssertEqual(subject.pokemonNameLabel.text, "Pikachu")
+class ViewControllerTests: QuickSpec {
+    override func spec() {
+        describe("ViewController") {
+            var subject: ViewController!
+            var mockNavigator: MockNavigator!
+            var mockViewModel: MockExampleViewModel!
+            
+            beforeEach {
+                subject = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController
+                
+                mockNavigator = MockNavigator()
+                mockViewModel = MockExampleViewModel()
+                
+                subject.navigator = mockNavigator
+                subject.viewModel = mockViewModel
+            }
+            
+            describe("viewDidLoad") {
+                beforeEach {
+                    mockViewModel.stub(function: "calculate", return: 10)
+                    _ = subject.view
+                }
+                
+                it("sets a label") {
+                    expect(subject.someLabel.text).to(equal("This is some text"))
+                }
+                
+                it("sets a label based upon a view model") {
+                    expect(subject.thirdLabel.text).to(equal("your number is 10"))
+                }
+            }
+            
+            context("when tapping a button") {
+                beforeEach {
+                    _ = subject.view
+                    subject.changeTextButton.sendActions(for: .touchUpInside)
+                }
+                
+                it("sets text") {
+                    expect(subject.otherLabel.text).to(equal("This is different text"))
+                }
+            }
+            
+            context("when tapping a different button") {
+                beforeEach {
+                    _ = subject.view
+                    subject.otherButton.sendActions(for: .touchUpInside)
+                }
+                
+                it("calls the navigator") {
+                    expect(mockNavigator.invoked(function: "show", with: [subject, "otherVC"])).to(beTrue())
+                }
+            }
+            
+            context("when tapping the network button") {
+                beforeEach {
+                    _ = subject.view
+                    subject.viewModel.pokemonName = "Pikachu"
+                    subject.pokemonIndex.text = "5"
+                    subject.networkButton.sendActions(for: .touchUpInside)
+                }
+                
+                it("asks the view model to make a call") {
+                    expect(mockViewModel.invoked(function: "makeCall", with: [subject, "5"])).to(beTrue())
+                }
+                
+                it("sets the label for the pokemon name") {
+                    // TODO: Capture the first argument of makeCall and call onSuccess on it
+                    
+                    subject.onSuccess()
+                    expect(subject.pokemonNameLabel.text).to(equal("Pikachu"))
+                }
+            }
+        }
     }
 }
 
+    
 class MockNavigator: Navigator, Mock {
+    
     var moxie = Moxie()
     
     override func show(caller: UIViewController, destination: String) {
+        
         record(function: "show", wasCalledWith: [caller, destination])
+        
     }
 }
 
 class MockExampleViewModel: ExampleViewModel, Mock {
+    
     var moxie = Moxie()
     
     override func calculate(forNumber number: Int) -> Int {
